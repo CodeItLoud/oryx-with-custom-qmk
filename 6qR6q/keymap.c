9201d3cee0a2427656721cc3692a8e4ea48ed773
CodeItLoud <97024129+CodeItLoud@users.noreply.github.com>
2026-06-06 21:10:28 +0200

    Do not remember H key itself

--- 6qR6q/keymap.c
+++ 6qR6q/keymap.c
@@ -228,7 +228,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
                             uint8_t* remembered_mods) {
     switch (keycode) {
-        case KC_H:
+        case MT(MOD_RGUI, KC_H):
             return false;  // Ignore and handle manually in process_record_user()
     }
 
