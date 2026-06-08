08a5680c105548951cbf09e63d7df5b4f4ca00d2
CodeItLoud <97024129+CodeItLoud@users.noreply.github.com>
2026-06-08 08:29:36 +0200

    try fix

--- 6qR6q/keymap.c
+++ 6qR6q/keymap.c
@@ -205,7 +205,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // Custom QMK starts
     case MT(MOD_RGUI, KC_H):
         if (record->tap.count) {
-          if (record->event.pressed)) {
+          if (record->event.pressed) {
             if (get_last_keycode() == MT(MOD_LALT, KC_S)) {
               SEND_STRING(/*s*/"ch");
             } else {
