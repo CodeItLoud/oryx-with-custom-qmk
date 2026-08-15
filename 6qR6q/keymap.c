06da7e93b6fb3545e46d855d4c7736a3b479c8cd
CodeItLoud <97024129+CodeItLoud@users.noreply.github.com>
2026-08-15 21:55:11 +0200

    fix, add S to history if O and E were typed subsequently

--- 6qR6q/keymap.c
+++ 6qR6q/keymap.c
@@ -237,8 +237,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // index 0 is two keys ago; index 1 is the immediate last key
 
         if (key_history[0] == KC_O && key_history[1] == MT(MOD_LCTL, KC_E)) {
-          SEND_STRING(was_caps_word_on ? "E" : "e");
-          add_to_history(MT(MOD_LCTL, KC_E));
+          SEND_STRING(was_caps_word_on ? "S" : "s");
+          add_to_history(MT(MOD_LALT, KC_S));
         } else if (key_history[0] == KC_O && key_history[1] == KC_A) {
           SEND_STRING(was_caps_word_on ? "S" : "s");
           add_to_history(MT(MOD_LALT, KC_S));
