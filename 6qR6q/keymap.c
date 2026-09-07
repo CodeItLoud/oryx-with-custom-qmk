c18a0568c533e90dc2a61cce599ec23e10b63aa2
CodeItLoud <97024129+CodeItLoud@users.noreply.github.com>
2026-09-07 20:57:06 +0200

    Exclude tap-hold thumb keys from chordal hold

--- 6qR6q/keymap.c
+++ 6qR6q/keymap.c
@@ -87,6 +87,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
+// Custom QMK starts
+// Exempt layer 0's five layer-tap thumb keys from Chordal Hold.
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_voyager(
+  'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+  'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+  'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
+  'L', 'L', 'L', 'L', '*', '*', '*', '*', 'R', 'R', 'R', 'R',
+                      'L', 'L',           'R', '*'
+);
+
 
 
 
